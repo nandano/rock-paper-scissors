c02cf6de592d3ab66d308b0ce6a4f1cd5b2ac1f5
nandano <193489667+nandano@users.noreply.github.com>
2025-04-29 18:20:34 +0530

    Modify getComputerChoice to return a string

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    const computerChoiceNumber = Math.floor(Math.random() * 3) + 1;
+    let computerChoice;
+    if (computerChoiceNumber === 1) {
+        computerChoice = 'rock';
+    } else if (computerChoiceNumber === 2) {
+        computerChoice = 'paper'
+    } else {
+        computerChoice = 'scissors';
+    }
     return computerChoice;
 }
 
